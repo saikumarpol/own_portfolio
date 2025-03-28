6b6fc3cad71739fc72527d4b8862c3033afd1b41
saikumarpola <92100989+saikumarpol@users.noreply.github.com>
2025-03-28 11:36:52 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -25,7 +25,7 @@ const Header = ({ darkMode, toggleDarkMode }) => {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" sx={{ flexGrow: 1, textDecoration: "none", color: "inherit" }} component={Link} to="/">
-            My Portfolio
+            My Portfolio 2
           </Typography>
           <Box sx={{ display: { xs: "none", md: "block" } }}>
             {menuItems.map((item) => (
